96f0a8b61489ebf2163834dfd5dc312be6304f26
gustavovaler <gustavodevaler@gmail.com>
2021-05-30 02:05:58 -0300

    check

--- handler.py
+++ handler.py
@@ -4,7 +4,7 @@ import json
 def hello(event, context):
     body = {
         "message": "Este servidor de prueba!!",
-        "input": event,
+        
     }
 
     response = {"statusCode": 200, "body": json.dumps(body)}
